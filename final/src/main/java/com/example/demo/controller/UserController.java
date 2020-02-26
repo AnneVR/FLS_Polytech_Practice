60ffb58be36b6698f92524f81bb50882abf8dd85
Anne <adudina99@gmail.com>
2020-02-26 17:38:38 +0300

    Beta version of backend
    0.1.0 version

---
+++ final/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController
+{
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Get all users list.
+     *
+     * @return the list
+     */
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Gets users by id.
+     *
+     * @param userId the user id
+     * @return the users by id
+     */
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId)
+            throws ResourceNotFoundException {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+        return ResponseEntity.ok().body(user);
+    }
+
+}
