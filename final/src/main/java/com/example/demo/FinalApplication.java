7d748b5d667da65f5a6699f71d18e2f4df745872
Anne <adudina99@gmail.com>
2020-03-21 19:13:08 +0300

    Add frontend support, jar creation and cors

--- final/src/main/java/com/example/demo/FinalApplication.java
+++ final/src/main/java/com/example/demo/FinalApplication.java
@@ -2,11 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FinalApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 }
