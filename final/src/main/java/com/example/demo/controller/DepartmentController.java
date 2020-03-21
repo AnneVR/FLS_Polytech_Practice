2c2914dd1ee497e491e8cef50b372dbc349512e0
JeRusakow <yegorsr@yandex.ru>
2020-03-21 23:24:15 +0300

    Bugfix
    * api/v1/departments not working fixed

--- final/src/main/java/com/example/demo/controller/DepartmentController.java
+++ final/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -5,15 +5,11 @@ import com.example.demo.model.Department;
 import com.example.demo.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 //@CrossOrigin(origins = "http//localhost:5432")
 @RequestMapping("/api/v1")
 public class DepartmentController {
