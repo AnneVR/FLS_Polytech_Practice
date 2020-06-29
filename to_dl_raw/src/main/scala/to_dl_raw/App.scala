5ec26e4679fa774945b0a2222644e2ea4d0854fd
aleksey <alexlyachko@gmail.com>
2020-06-29 22:15:21 +0300

    hhh

--- to_dl_raw/src/main/scala/to_dl_raw/App.scala
+++ to_dl_raw/src/main/scala/to_dl_raw/App.scala
@@ -14,7 +14,7 @@ object App {
     session.sparkContext.setLogLevel("Warn")
 
     val data = session.read.option("header", "true")
-                            .csv("hdfs://" + args(0) +  "/data/application_data.csv")
+                            .csv("hdfs://" + args(0) +  "/sba/application_data.csv")
 
     data.write.mode("Overwrite").saveAsTable("dl_raw_full")
 
