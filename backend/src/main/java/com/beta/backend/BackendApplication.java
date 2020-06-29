7ff139435030a626ab9f646cd950e1eb599cc7d1
JeRusakow <yegorsr@yandex.ru>
2020-06-29 16:08:51 +0300

    Initial backend commit

---
+++ backend/src/main/java/com/beta/backend/BackendApplication.java
@@ -0,0 +1,17 @@
+package com.beta.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
