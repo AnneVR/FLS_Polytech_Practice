e4283ec17892007033ffffd85f61898fe2ea8c5a
alex <alexander.korichev@gmail.com>
2020-07-05 16:34:42 +0300

    trying to fix

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into test_table (name) values ('John');
-insert into test_table (name) values ('Adam');
-insert into test_table (name) values ('Mary');
-insert into test_table (name) values ('Eve');
+insert into hello (name) values ('John');
+insert into hello (name) values ('Adam');
+insert into hello (name) values ('Mary');
+insert into hello (name) values ('Eve');
